f6220068d15d3c72dcf991387d32368e48a7338c
shuaibansari123 <shuaibansari4044@gmail.com>
2024-10-03 12:03:39 +0530

    Added CKEditor field for fully customized formatted question, create all user template and user-answer teplate with additional logics

--- ApptituteBackend/test_app/admin.py
+++ ApptituteBackend/test_app/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from .models import UserCandidate, Question , Answer
+from django.contrib import admin
+from .models import Question
+from ckeditor.widgets import CKEditorWidget
+from django import forms
 
-# Register your models here.
-admin.site.register(Question)
+class QuestionAdminForm(forms.ModelForm):
+    question_text = forms.CharField(widget=CKEditorWidget())
+
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+class QuestionAdmin(admin.ModelAdmin):
+    form = QuestionAdminForm
+
+
 admin.site.register(Answer)
 admin.site.register(UserCandidate)
+admin.site.register(Question)
